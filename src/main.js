f4a39c78d5b598b60f4d7953ae70d1026497a922
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 19:09:33 -0300

    feat: adiciona atributo a questão e captura com JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
